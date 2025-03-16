944d733ba2e16248ccf07a9a6cebdea7de82a5cb
Abdul Samad <junaid.abdulsamad@gmail.com>
2025-03-16 22:27:38 +0500

    commiting my logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,15 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-
-import LoginScreen from './pages/login';
-import SignupScreen from './pages/signup';
-import Recipe from './Recipe.jsx';
-import Search from './Search.jsx';
+import { useState, useEffect } from 'react'
+import Recipe from './Recipe.jsx'
+import Search from './Search.jsx'
 
 function App() {
-  const [login, setLogin] = useState(true);
 
   return (
-    <div className="App">
-      {login ? (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Router>
-            <Routes>
-              <Route path="/login" element={<LoginScreen setLogin={setLogin}/>} />
-              <Route path="/signup" element={<SignupScreen />} />
-            </Routes>
-          </Router>
-        </header>
-      ) : (
-        <div className="page">
-          <Search />
-          <Recipe />
-        </div>
-      )}
+    <div className="page">
+      <Search/>
+      <Recipe/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/utils/validations.jsx
+++
@@ -1,8 +0,0 @@
-export const validateEmail = (email) => {
-    return /\S+@\S+\.\S+/.test(email);
-  };
-  
-  export const validatePassword = (password) => {
-    return password.length >= 6;
-  };
-  
