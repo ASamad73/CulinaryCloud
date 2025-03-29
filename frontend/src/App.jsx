5a0ff66dc870d768067dbc2cfa47d629ba56f086
Abdul Samad <junaid.abdulsamad@gmail.com>
2025-03-30 03:09:50 +0500

    post commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState, useEffect } from 'react'
 import Recipe from './Recipe.jsx'
 import Search from './Search.jsx'
+import Post from './Post.jsx'
+
 
 function App() {
+  const [post, setPost]=useState(false);
 
   return (
-    <div className="page">
-      <Search/>
-      <Recipe/>
+    <div className="screen">
+      <div className="page">
+        {!post && 
+        (
+          <>
+            <Search/>
+            <Recipe/>
+          </>
+        )}
+        {post && <Post/>}
+      </div>
+      <div className="header-right">
+        <i className="fa-solid fa-plus" onClick={()=>setPost(true)}></i>
+    </div>
     </div>
   )
 }
