f4683f95177c616e4d3cbe2ec2776ab9e85ec36e
Ahmad_Jawwad <ahmadjawwad001@gmail.com>
2025-05-04 17:18:48 +0500

    Dock loc

--- Backend/middleware/passportConfig.js
+++ Backend/middleware/passportConfig.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `https://backend‑nameless‑cloud‑4888.fly.dev/api/auth/google/callback`
+      callbackURL: "https://backend‑nameless‑cloud‑4888.fly.dev/api/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
