cf7a0552072881f54ebd71963cd03075b15bd54c
Ahmad_Jawwad <ahmadjawwad001@gmail.com>
2025-05-04 21:46:36 +0500

    i understand everything now

--- Backend/server.js
+++ Backend/server.js
@@ -17,8 +17,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL,
-    'https://se-project-bice.vercel.app'
+    process.env.FRONTEND_URL
   ],
   // credentials: true
 }));
